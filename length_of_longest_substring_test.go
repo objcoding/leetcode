f67ebaaf240a76829cb0c295cceddea716a3c27a
zch <zhangchenghui.dev@gmail.com>
2018-09-15 01:20:37 +0800

    test file

---
+++ length_of_longest_substring_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestLengthOfLongestSubstring1(t *testing.T) {
+	t.Log(LengthOfLongestSubstring1("dsfdsfdsfsdfdsgh"))
+}
---
+++ two_sum_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "testing"
+
+func TestTwoSum1(t *testing.T) {
+	t.Log(TwoSum1([]int{2, 7, 11, 15}, 9))
+}
+
+func TestTwoSum2(t *testing.T) {
+	t.Log(TwoSum2([]int{2, 7, 11, 15}, 9))
+}
+
+func TestTwoSum3(t *testing.T) {
+	t.Log(TwoSum3([]int{2, 7, 11, 15}, 9))
+}
